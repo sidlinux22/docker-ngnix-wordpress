5de9341b1f3e4e328e07a6daa7d6b1b4478080e9
sshar43 <ssharma2@walmartlabs.com>
2015-10-15 14:43:28 +0530

    docker config

---
+++ start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+  # Download nginx helper plugin
+  curl -O `curl -i -s https://wordpress.org/plugins/nginx-helper/ | egrep -o "https://downloads.wordpress.org/plugin/[^']+"`
+  unzip -o nginx-helper.*.zip -d /usr/share/nginx/www/wp-content/plugins
+  chown -R www-data:www-data /usr/share/nginx/www/wp-content/plugins/nginx-helper
+
+  # Activate nginx plugin once logged in
+  cat << ENDL >> /usr/share/nginx/www/wp-config.php
+\$plugins = get_option( 'active_plugins' );
+if ( count( \$plugins ) === 0 ) {
+  require_once(ABSPATH .'/wp-admin/includes/plugin.php');
+  \$pluginsToActivate = array( 'nginx-helper/nginx-helper.php' );
+  foreach ( \$pluginsToActivate as \$plugin ) {
+    if ( !in_array( \$plugin, \$plugins ) ) {
+      activate_plugin( '/usr/share/nginx/www/wp-content/plugins/' . \$plugin );
+    }
+  }
+}
+ENDL
+
+chown www-data:www-data /usr/share/nginx/www/wp-config.php
+
+# start all the services
+/usr/local/bin/supervisord -n
